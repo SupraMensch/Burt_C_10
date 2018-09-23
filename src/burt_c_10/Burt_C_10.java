8882f9856beb25ae7a86c9231da29dea6deb9e37
Christopher Burt <cwadeburt@gmail.com>
2018-09-22 23:56:03 -0500

    Project parameters added

---
+++ src/burt_c_10/Burt_C_10.java
@@ -0,0 +1,23 @@
+//  Christopher Burt
+//  Project # 10
+
+// A prime number is a number that is only evenly divisible by itself and 1.  You can refer to programming challenge 13 on page 316 in the textbook for more details.
+
+// Write a Java program that finds prime numbers:
+// 1. Create a method called isPrime that accepts an Integer value and returns true if it is prime and false if it is not.
+// 2. In your main method, write a loop that that calls the method and passes it values from 1 to 100.
+// 3. This loop should display all of the values that are prime.
+package burt_c_10;
+
+
+public class Burt_C_10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
